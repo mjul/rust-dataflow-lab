1dac8b9641144ada22c9ab22a6e9036beb1873cc
Martin Jul <martin@mjul.com>
2024-02-28 22:48:39 +0100

    Add example with pipeline of mappings

--- src/lib.rs
+++ src/lib.rs
@@ -1,7 +1,8 @@
-
 extern crate timely;
 
+use timely::dataflow::InputHandle;
 use timely::dataflow::operators::*;
+use timely::dataflow::operators::{Exchange, Input, Inspect, Probe};
 
 // This is the simplest example from the documentation, stream numbers through a print data flow.
 fn getting_started() {
@@ -9,6 +10,30 @@ fn getting_started() {
         (0..10).to_stream(scope)
             .inspect(|x| println!("seen: {:?}", x));
     });
+}
+
+fn linear_steps() {
+    let config = timely::execute::Config::process(4);
+    timely::execute(config, |worker| {
+        let index = worker.index();
+        let mut input = InputHandle::new();
+        let probe = worker.dataflow(|scope| {
+            scope.input_from(&mut input)
+                // exchange has to be used to make sure that the data is shuffled between workers
+                .exchange(|x| *x)
+                .map(|x| x * x)
+                .map(|x| x + 1)
+                .inspect(move |x| println!("worker {}:\t $x^2+1 = {}$", index, x))
+                .probe()
+        });
+        for round in 0..10 {
+            // Only worker 0 sends data
+            if index == 0 {
+                input.send(round);
+            }
+            input.advance_to(round + 1);
+        }
+    }).unwrap();
 }
 
 
@@ -19,6 +44,12 @@ mod tests {
     #[test]
     fn getting_started_can_run() {
         getting_started();
+        assert_eq!(true, true);
+    }
+
+    #[test]
+    fn linear_steps_can_run() {
+        linear_steps();
         assert_eq!(true, true);
     }
 
